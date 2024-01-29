1356a4de7b189752dd52c26b9617186577badeef
Jyoti Kumari <jyokumar1@publicisgroupe.net>
2024-01-29 16:37:09 +0530

    first commit

---
+++ ProductsAssignment/ProductsAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsAssignmentApp.swift
+//  ProductsAssignment
+//
+//  Created by Jyoti Kumari on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductsAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
